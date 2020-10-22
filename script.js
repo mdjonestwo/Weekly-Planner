7c2fa1462c27a19898425f48475ff69e545f983f
Sage <mdjones2@protonmail.com>
2020-10-22 01:40:50 -0400

    created table for layout

---
+++ script.js
@@ -0,0 +1,10 @@
+var m = moment()
+$('#currentDay').text(m)
+
+var hours = ["8:00", "9:00", "10:00","11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00"]
+
+for(var i = 0; i<hours.length; i++){
+    $('#time').forEach
+    
+}
+
