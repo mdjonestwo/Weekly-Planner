ece5bc55d9847375bee531369a20fc94c0fc749d
Sage <mdjones2@protonmail.com>
2020-10-22 22:43:32 -0400

    time slots appear with loop

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ var m = moment()
 $('#currentDay').text(m)
 
 var hours = ["8:00", "9:00", "10:00","11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00"]
+var $table = $('<table>')
+var $tableRow = $('<tr>') 
+var $timeSlot = $('<td>').addClass("time")
+var $scheduleSlot = $('<td>').addClass("schedule")
+var $trackerSlot = $('<td>').addClass("timeTracker")
 
-for(var i = 0; i<hours.length; i++){
-    $('#time').forEach
+$('.container').append($table)
+
+
+hours.forEach(function(element, index) {
+    $table.append($tableRow).push(index) 
+    $tableRow.append($timeSlot) 
+    $timeSlot.text(element)
     
-}
+
+})
 
