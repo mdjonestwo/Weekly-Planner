c9987fc822619de7cfb965bdb3b6c4fc5b0f4538
Sage <mdjones2@protonmail.com>
2020-10-22 23:34:43 -0400

    comments added to JS

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-var m = moment()
-$('#currentDay').text(m)
 
+//Moment.js Variable Assigments 
+var m = moment().format('MMMM Do YYYY, h:mm:ss a')
+var mFromHour = moment().startOf('hour').fromNow();    
+
+
+//Dom Variable Assignments 
 var hours = ["8:00", "9:00", "10:00","11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00"]
 var $table = $('<table>')
 var $tableRow = $('<tr>') 
@@ -11,13 +15,12 @@ var inputField = $('<input>').attr('type', 'text')
 var val = $scheduleSlot.attr('input')
 var saveButtonSlot = $('<td>').append($('<button>' + "Save" + '</button>' ))
 
-//$scheduleSlot.append($('<input>')).attr('input','')
 
 
-
+$('#currentDay').text(m)
 $('.container').append($table)
 
-
+//Create Planner
 hours.forEach(function(element, index) {
     $table.append($tableRow).push(index) 
     $tableRow.append($timeSlot) 
@@ -26,6 +29,11 @@ hours.forEach(function(element, index) {
     $scheduleSlot.append(inputField)
     $tableRow.append(saveButtonSlot)
     $tableRow.append($trackerSlot)
-    
-
 })
+
+//Track time 
+function timeTrack(){
+    if(m < )
+
+
+}
